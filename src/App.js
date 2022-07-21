7a995269379c1bf6852b75aa0b9ab09e34b531b9
mtripaldi03 <108495657+mtripaldi03@users.noreply.github.com>
2022-07-21 20:34:51 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Test from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
-import ItemCount from './components/ItemCount';
 function App() {
-  const onAddItem = (count) => {
-
-  };
+ 
   return (
     <>
     <Test/>
     <ItemListContainer greeting="Bienvenido a la tienda"/>
-    <ItemCount onAdd={onAddItem}/>
+    
     </>
   );
 }
